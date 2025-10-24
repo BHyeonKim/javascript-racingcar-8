1fca51f5609c97d64520ae529a529971c60b0e2e
BHyeonKim <rlaqhguse@gmail.com>
2025-10-24 16:30:25 +0900

    test(validator): Add tests for validateNoDuplication method

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -55,4 +55,44 @@ describe('Validator Class', () => {
       ).not.toThrow();
     });
   });
+
+  describe('validateNoDuplication Method', () => {
+    it('should throw an error if input is not an array', () => {
+      const input = 'test';
+
+      expect(() => Validator.validateNoDuplication(input)).toThrow(
+        ERROR_MESSAGE.NOT_ARRAY,
+      );
+    });
+
+    it('should throw an error if duplication exists in array with string items', () => {
+      const input = ['test1', 'test2', 'test3', 'test1'];
+
+      expect(() => Validator.validateNoDuplication(input)).toThrow(
+        ERROR_MESSAGE.DUPLICATED_ARRAY_ITEM,
+      );
+    });
+
+    it('should throw an error if duplication exists in array with number items', () => {
+      const input = [1, 2, 3, 1];
+
+      expect(() => Validator.validateNoDuplication(input)).toThrow(
+        ERROR_MESSAGE.DUPLICATED_ARRAY_ITEM,
+      );
+    });
+
+    it('should throw an error if duplication exists in array with mixed type of items', () => {
+      const input = [1, 2, 'test1', 1];
+
+      expect(() => Validator.validateNoDuplication(input)).toThrow(
+        ERROR_MESSAGE.DUPLICATED_ARRAY_ITEM,
+      );
+    });
+
+    it('should not throw an error if there is no duplication', () => {
+      const input = ['test1', 'test2', 'test3'];
+
+      expect(() => Validator.validateNoDuplication(input)).not.toThrow();
+    });
+  });
 });
