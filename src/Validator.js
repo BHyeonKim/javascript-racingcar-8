4e711fef343180f1986034941a05732faca04dd0
BHyeonKim <rlaqhguse@gmail.com>
2025-10-24 15:22:23 +0900

    feat: Implement validator class

---
+++ src/Validator.js
@@ -0,0 +1,47 @@
+import ERROR_MESSAGE from './constants/error.js';
+
+class Validator {
+  static validateMoreThanOneCharacter(string) {
+    if (string.length < 1) {
+      throw new Error(ERROR_MESSAGE.EMPTY_STRING);
+    }
+  }
+
+  static validateLessThanFiveCharacter(string) {
+    if (string.length > 5) {
+      throw new Error(ERROR_MESSAGE.EXCEED_FIVE_CHARACTER);
+    }
+  }
+
+  static validateNoDuplication(arr) {
+    Validator.validateIsArray(arr);
+
+    const set = new Set(arr);
+
+    if (arr.length !== set.size) {
+      throw new Error(ERROR_MESSAGE.DUPLICATED_ARRAY_ITEM);
+    }
+  }
+
+  static validateNumber(number) {
+    if (Number.isNaN(number)) {
+      throw new Error(ERROR_MESSAGE.NOT_NUMBER);
+    }
+  }
+
+  static validatePositiveNumber(number) {
+    Validator.validateNumber(number);
+
+    if (number < 1) {
+      throw new Error(ERROR_MESSAGE.NOT_POSITIVE_NUMBER);
+    }
+  }
+
+  static validateIsArray(arr) {
+    if (!Array.isArray(arr)) {
+      throw new Error(ERROR_MESSAGE.NOT_ARRAY);
+    }
+  }
+}
+
+export default Validator;
---
+++ src/constants/error.js
@@ -0,0 +1,10 @@
+const ERROR_MESSAGE = {
+  EMPTY_STRING: '문자열의 길이가 0입니다.',
+  EXCEEDED_FIVE_CHARACTER: '문자열의 길이가 5개를 초과합니다.',
+  DUPLICATED_ARRAY_ITEM: '배열의 아이템이 중복되었습니다.',
+  NOT_NUMBER: '숫자가 아닙니다.',
+  NOT_POSITIVE_NUMBER: '양수가 아닙니다.',
+  NOT_ARRAY: '배열이 아닙니다.',
+};
+
+export default ERROR_MESSAGE;
