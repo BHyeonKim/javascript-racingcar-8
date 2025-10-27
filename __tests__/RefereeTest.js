55735b7cb62e9865c9497c5528977c0aa6430c9c
BHyeonKim <rlaqhguse@gmail.com>
2025-10-27 11:39:14 +0900

    test(referee): Add tests for playRound method

--- __tests__/RefereeTest.js
+++ __tests__/RefereeTest.js
@@ -1,4 +1,5 @@
 import ERROR_MESSAGE from '../src/constants/error.js';
+import GAME_CONSTANT from '../src/constants/gameConstant.js';
 import Car from '../src/models/Car.js';
 import Referee from '../src/models/Referee.js';
 
@@ -82,6 +83,94 @@ describe('Referee class', () => {
 
       testCases.forEach((testCase) => {
         expect(() => Referee.getWinningCars(testCase)).toThrow(
+          ERROR_MESSAGE.NOT_CAR,
+        );
+      });
+    });
+  });
+  describe('playRound Method', () => {
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should not move any cars when all random numbers are below threshold', () => {
+      const cars = [new Car('test1'), new Car('test2'), new Car('test3')];
+
+      const spiedGenerateRandomNumber = jest
+        .spyOn(Referee, 'generateRandomNumber')
+        .mockReturnValueOnce(GAME_CONSTANT.RANDOM_START)
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD - 1)
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD - 1);
+
+      Referee.playRound(cars);
+
+      expect(cars[0].distance).toBe(0);
+      expect(cars[1].distance).toBe(0);
+      expect(cars[2].distance).toBe(0);
+
+      expect(spiedGenerateRandomNumber).toHaveBeenCalledTimes(3);
+    });
+
+    it('should move all cars when all random numbers are at or above threshold', () => {
+      const cars = [new Car('test1'), new Car('test2'), new Car('test3')];
+
+      const spiedGenerateRandomNumber = jest
+        .spyOn(Referee, 'generateRandomNumber')
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD)
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD + 1)
+        .mockReturnValueOnce(GAME_CONSTANT.RAMDOM_END);
+
+      Referee.playRound(cars);
+
+      expect(cars[0].distance).toBe(1);
+      expect(cars[1].distance).toBe(1);
+      expect(cars[2].distance).toBe(1);
+
+      expect(spiedGenerateRandomNumber).toHaveBeenCalledTimes(3);
+    });
+
+    it('should maintain previous distances and increment correctly', () => {
+      const cars = [
+        new Car('test1', 3),
+        new Car('test2', 5),
+        new Car('test3', 0),
+      ];
+
+      const spiedGenerateRandomNumber = jest
+        .spyOn(Referee, 'generateRandomNumber')
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD)
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD - 1)
+        .mockReturnValueOnce(GAME_CONSTANT.THRESHOLD);
+
+      Referee.playRound(cars);
+
+      expect(cars[0].distance).toBe(4);
+      expect(cars[1].distance).toBe(5);
+      expect(cars[2].distance).toBe(1);
+
+      expect(spiedGenerateRandomNumber).toHaveBeenCalledTimes(3);
+    });
+
+    it('should throw an error if input is not an array', () => {
+      const testCases = [' ', 5, false, {}];
+
+      testCases.forEach((testcase) => {
+        expect(() => Referee.playRound(testcase)).toThrow(
+          ERROR_MESSAGE.NOT_ARRAY,
+        );
+      });
+    });
+
+    it('should throw an error if input is not array of cars', () => {
+      const testCases = [
+        [{}, {}, {}],
+        [5, 6, 7],
+        [' ', ' '],
+        [false, true],
+      ];
+
+      testCases.forEach((testCase) => {
+        expect(() => Referee.playRound(testCase)).toThrow(
           ERROR_MESSAGE.NOT_CAR,
         );
       });
